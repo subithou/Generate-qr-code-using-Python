25a00774f3bfaca3ff6f494ff3a5bf8a8debafef
Subith O U <57960146+subithou@users.noreply.github.com>
2022-02-16 12:38:43 +0530

    Python File for create

---
+++ qrcode.py
@@ -0,0 +1,19 @@
+# Import QRCode from pyqrcode
+import pyqrcode
+import png
+from pyqrcode import QRCode
+
+  
+
+
+# String which represents the QR code
+s = " Happy Valentine's Day"
+
+# Generate QR code
+url = pyqrcode.create(s)
+  
+# Create and save the svg file naming "myqr.svg"
+# url.svg("qrcode.svg", scale = 8)
+  
+# Create and save the png file naming "myqr.png"
+url.png('qrcode.jpg', scale = 45)
